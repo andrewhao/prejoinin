f7df7f6a2a29a7ebca631c12de13c2f19c8ee9d8
Andrew Hao <andrewhao@gmail.com>
2016-07-24 11:32:32 +0900

    Play with some examples to move a thing around in the DOM.

--- samples/Counter.elm
+++ samples/Counter.elm
@@ -1,40 +1,57 @@
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, Attribute)
+import Html.Attributes exposing (style, id)
 import Html.App as Html
 import Html.Events exposing (onClick)
-
+import Dict exposing (Dict)
+import Debug exposing (log)
 
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 -- MODEL
-
-type alias Model = Int
+type alias Model =
+  { x : Int
+  , y : Int }
 
 model : Model
-model =
-  0
-
+model = Model 0 0
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = MoveUp | MoveDown | MoveLeft | MoveRight
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
+    MoveUp ->
+      Model model.x (model.y + 1)
+    MoveDown ->
+      Model model.x (model.y - 1)
+    MoveLeft ->
+      Model (model.x - 1) model.y
+    MoveRight ->
+      Model (model.x + 1) model.y
 
 -- VIEW
+
+myStyle : Model -> Attribute msg
+myStyle model =
+  style
+  [ ("position", "absolute")
+  , ("color", "red")
+  , ("left", (toString model.x ++ "px"))
+  , ("top", (toString model.y ++ "px"))
+  ]
 
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ button [ onClick MoveDown ] [ text "-" ]
+    , button [ onClick MoveLeft ] [ text "<-" ]
     , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick MoveUp ] [ text "+" ]
+    , button [ onClick MoveRight ] [ text "->" ]
+    , div [ (id "container"), (style [ ("position", "relative") ] ) ]
+      [ div [ myStyle model ]
+        [ text "hello" ] ]
     ]
