70e260e607866a2efd719b16ef84b4b453da7420
Andrew Hao <andrew@carbonfive.com>
2017-01-03 09:02:04 -0800

    [wip] Adding more powerful decode behavior on JSON with full Row and Column and SignupSlot detail

--- App.elm
+++ App.elm
@@ -9,40 +9,94 @@ import Debug exposing (..)
 type alias Model =
   { sheetId : String
   , signupCount : Int
+  , title : String
+  , description : String
+  , rows: List Row
+  , columns: List Column
+  , signupSlots: List SignupSlot
   }
 
 init : String -> (Model, Cmd Msg)
 init sheetId =
-  ( Model sheetId 0
+  ( Model sheetId
   , getSheetDetails sheetId
   )
 
 -- UPDATE
 type Msg
-  = RefreshSheet
-  | ReceiveSheetDetails (Result Http.Error Int)
+  = FetchSheet
+  | ReceiveSheetDetails (Result Http.Error SheetJSONResponse)
+  | ChangeSheetID String
+
+type alias LegacySheetJSONResponse =
+  { signupCount : Int
+  , title : String
+  , description : String
+  }
+
+type alias SheetJSONResponse =
+  { title : String
+  , description : String
+  , rows : List Row
+  , columns : List Column
+  , signupSlots : List SignupSlot
+  }
+
+type alias Row =
+  { id : Int
+  , position : Int
+  , value : String
+  }
+
+type alias Column =
+  { id : Int
+  , position : Int
+  , value : String
+  }
+
+type alias SignupSlot =
+  { id : Int
+  , rowId : Int
+  , columnId : Int
+  , maxSignups : Int
+  , closed : Bool
+  }
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    RefreshSheet ->
+    FetchSheet ->
       (model, getSheetDetails model.sheetId)
 
-    ReceiveSheetDetails (Ok newSignupCount) ->
-      (Model model.sheetId newSignupCount, Cmd.none)
+    ReceiveSheetDetails (Ok jsonResponse) ->
+      (Model model.sheetId
+             0
+             jsonResponse.title
+             jsonResponse.description
+             jsonResponse.rows
+             jsonResponse.columns
+             jsonResponse.signupSlots
+      , Cmd.none)
 
     ReceiveSheetDetails (Err err) ->
       Debug.log (toString err)
       (model, Cmd.none)
 
+    ChangeSheetID newSheetId ->
+      ({ model | sheetId = newSheetId }
+      , Cmd.none)
+
 -- VIEW
 view : Model -> Html Msg
 view model =
   div []
-    [ h2 [] [text model.sheetId]
-    , button [ onClick RefreshSheet ] [ text "Refresh" ]
+    [ input [ placeholder "Sheet ID", onInput ChangeSheetID ] []
+    , h2 [] [text model.sheetId]
+    , button [ onClick FetchSheet ] [ text "Fetch" ]
     , br [] []
     , p [] [text (model.signupCount |> toString)]
+    , h1 [] [text (model.title)]
+    , p [] [text (model.description)]
     ]
 
 -- SUBSCRIPTIONS
@@ -64,13 +118,42 @@ getSheetDetails sheetId =
     Http.send ReceiveSheetDetails (Http.get url fetchSheetDetails)
 
 
-fetchSheetDetails : Decoder Int
+fetchSheetDetails : Decoder SheetJSONResponse
 fetchSheetDetails =
-  at ["signups_count"] int
+  map5 SheetJSONResponse
+    (field "title" string)
+    (field "description" string)
+    (field "rows" decodeRows)
+    (field "columns" decodeColumns)
+    (field "signup_slots" decodeSignupSlots)
+
+decodeRows : Decoder (List Row)
+decodeRows =
+  map3 Row
+    (field "id" int)
+    (field "position" int)
+    (field "value" string)
+
+decodeColumns : Decoder (List Column)
+decodeColumns =
+  map3 Column
+    (field "id" int)
+    (field "position" int)
+    (field "value" string)
+
+
+decodeSignupSlots : Decoder (List SignupSlot)
+decodeSignupSlots =
+  map5 SignupSlot
+    (field "id" int)
+    (field "row_id" int)
+    (field "column_id" int)
+    (field "max_signups" int)
+    (field "closed" bool)
 
 main =
   Html.program
-    { init = init "sample_sheet"
+    { init = init "sheet1"
     , view = view
     , update = update
     , subscriptions = subscriptions
